62eb8a5a8a3bfba0d9b01eb07910a73a75c26f53
Steven Buxton <breathesteve@gmail.com>
2016-01-16 14:25:54 -0600

    sortable function

--- app.js
+++ app.js
@@ -1,12 +1,1 @@
-$('#square').draggable().html("Food")
-$('#drop').droppable({
-	drop: function(ui, event) {
-		$('#text').html("I'm not even kind of full");
-	}
-}).html("My stomach");
-
-$('#dro').droppable({
-	drop: function(ui, event) {
-		$('#text').html("I'm super full");
-	}
-}).html("Your stomach");
+$('ul').sortable();
