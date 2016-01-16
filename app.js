b2215481e683fdd7b8ecafa8693d3f90a89afc56
Steven Buxton <breathesteve@gmail.com>
2016-01-16 13:40:49 -0600

    eating example ui

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-$('#square').draggable().html("Drag me around.")
+$('#square').draggable().html("Food")
+$('#drop').droppable({
+	drop: function(ui, event) {
+		$('#text').html("I'm not even kind of full");
+	}
+}).html("My stomach");
+
+$('#dro').droppable({
+	drop: function(ui, event) {
+		$('#text').html("I'm super full");
+	}
+}).html("Your stomach");
